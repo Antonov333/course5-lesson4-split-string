624ed744460f195220a3b0d89df155b1d9f3f19b
Antonov333 <antonovsergei333@yahoo.com>
2023-09-28 19:29:48 +0300

    Simplest dividing of string composed

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -7,28 +11,32 @@ public class Main {
 
         /*
         Problem Solving plan
-
-        Form of software: single method
-
-        Choose Return type:
-
-        Choose Set of arguments:
-
-        Learn existing libraries possibly useful to solve the problem
-
+        Form of software: single method stringDivider
+        Choose Return type: List of strings
+        Choose Set of arguments: source string, length of part
          */
 
-
+        List<String> stringList = stringDivider("blablablabla", 6);
+        for (String s : stringList
+        ) {
+            System.out.println(s);
         }
 
-    public String[] stringDivider(String string, int numberOfParts){
-        String[] dividedString = new String[numberOfParts];
-        dividedString[0] = string;
+    }
 
-        // First let's create simplest cutting
+    public static List<String> stringDivider(String string, int lengthOfPart) {
+        List<String> outSetOfStrings = new ArrayList<>();
+        int nextCharIndex = 0;
+        int endOfPart = 0;
+        do {
+            endOfPart = nextCharIndex + lengthOfPart;
+            if (endOfPart > string.length()) {
+                endOfPart = string.length();
+            }
+            outSetOfStrings.add(string.substring(nextCharIndex, endOfPart));
+            nextCharIndex = nextCharIndex + lengthOfPart;
+        } while (nextCharIndex < string.length());
 
-
-
-        return dividedString;
+        return outSetOfStrings;
     }
 }
