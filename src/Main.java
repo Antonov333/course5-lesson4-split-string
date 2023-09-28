f89b5df676587efcb148e17e1a2c2c422fa92fd9
Antonov333 <antonovsergei333@yahoo.com>
2023-09-28 16:00:25 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!\n\nSolving problem on dividing a String");
+
+        /*
+        Problem Solving plan
+
+        Form of software: single method
+
+        Choose Return type:
+
+        Choose Set of arguments:
+
+        Learn existing libraries possibly useful to solve the problem
+
+         */
+
+
+        }
+
+    public String[] stringDivider(String string, int numberOfParts){
+        String[] dividedString = new String[numberOfParts];
+        dividedString[0] = string;
+
+        // First let's create simplest cutting
+
+
+
+        return dividedString;
+    }
+}
